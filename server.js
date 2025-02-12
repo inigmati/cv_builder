d03dca3d66cabb596138a87a31c82951fb885150
inigmati <amol.katkade@outlook.com>
2025-02-13 03:55:10 +0530

    stickies path

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-// Serve static files (optional, if you have HTML/CSS/JS)
-app.use(express.static('public'));
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Basic Route
+// Route for /
 app.get('/', (req, res) => {
-    res.send('Hello, Heroku!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Dynamic Port for Heroku
+// Route for /stickies
+app.get('/stickies', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'stickies.html'));
+});
+
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
